c90a00e4304ddfe0c4322faef6c30cd8de332b86
RETNUHTROP <0x00@6c6f6c.net>
2015-07-15 18:22:43 +0200

    pointer basics

---
+++ pointer_basics.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	char lead;
+	char *sidekick;
+
+	lead = 'A';
+	sidekick = &lead;
+
+	printf("About variable 'lead':\n");
+	printf("Size\t\t%ld\n", sizeof(lead));
+	printf("Contents\t%c\n", lead);
+	printf("Location\t%p\n", &lead);
+	printf("And variable 'sidekick':\n");
+	printf("Contents\t%p\n", sidekick);
+	return(0);
+}
